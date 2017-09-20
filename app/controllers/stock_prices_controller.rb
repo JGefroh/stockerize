fad6e6a773b84c3a1668eed1b2a25a6d8edc3617
Joseph Gefroh <joseph.gefroh@gmail.com>
2017-09-19 20:09:15 -1000

    Stock price list is ordered by date.

--- app/controllers/stock_prices_controller.rb
+++ app/controllers/stock_prices_controller.rb
@@ -4,7 +4,7 @@ class StockPricesController < ApplicationController
     if params.count == 2
       @stock_prices = @stock_prices.order(date: :desc).order('close_price_cents - open_price_cents desc')
     end
-    @stock_prices = @stock_prices.where(stock_id: params[:stock_id]) if params[:stock_id]
+    @stock_prices = @stock_prices.where(stock_id: params[:stock_id]).order(date: :desc) if params[:stock_id]
     @stock_prices = @stock_prices.where(ticker: params[:ticker]) if params[:ticker]
     @stock_prices = @stock_prices.limit(100)
     render json: @stock_prices, root: false
