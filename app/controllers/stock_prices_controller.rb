d3c7db75a54bb69e81ab234be06b83dd6ab429d6
Joseph Gefroh <joseph.gefroh@gmail.com>
2017-09-19 20:07:17 -1000

    Home page shows top 100 deltas.

--- app/controllers/stock_prices_controller.rb
+++ app/controllers/stock_prices_controller.rb
@@ -1,8 +1,12 @@
 class StockPricesController < ApplicationController
   def index
-    @stock_prices = StockPrice.all.limit(100)
+    @stock_prices = StockPrice.all
+    if params.count == 2
+      @stock_prices = @stock_prices.order(date: :desc).order('close_price_cents - open_price_cents desc')
+    end
     @stock_prices = @stock_prices.where(stock_id: params[:stock_id]) if params[:stock_id]
     @stock_prices = @stock_prices.where(ticker: params[:ticker]) if params[:ticker]
+    @stock_prices = @stock_prices.limit(100)
     render json: @stock_prices, root: false
   end
 end
