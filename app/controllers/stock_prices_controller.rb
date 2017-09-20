6cb27a240f61b92d1145c7a331a19c684fb68efb
Joseph Gefroh <joseph.gefroh@gmail.com>
2017-09-19 20:12:32 -1000

    Stop using deprecated count function.

--- app/controllers/stock_prices_controller.rb
+++ app/controllers/stock_prices_controller.rb
@@ -1,7 +1,7 @@
 class StockPricesController < ApplicationController
   def index
     @stock_prices = StockPrice.all
-    if params.count == 2
+    if params.to_h.count == 2
       @stock_prices = @stock_prices.order(date: :desc).order('close_price_cents - open_price_cents desc')
     end
     @stock_prices = @stock_prices.where(stock_id: params[:stock_id]).order(date: :desc) if params[:stock_id]
